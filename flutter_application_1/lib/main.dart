72230d10654d9f3dede8b950e2004bf85f37cb52
rebekaamorim <rebeka.amorim@etec.sp.gov.br>
2024-06-08 11:41:55 -0300

    funcionalidade perfil feita

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:flutter_application_1/perfil.dart';
+
+class Login {
+  final String email;
+  final String senha;
+  //construtor
+  const Login(this.email, this.senha);
+}
 
 void main() {
   runApp(const MainApp());
@@ -10,167 +17,198 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      home: Scaffold(
-          body: Container(
-            padding: EdgeInsets.all(20),
-            color: Colors.grey[900],
-            child: Column (children: [
-              Expanded(
-              child: SingleChildScrollView(
-              child:Column(children:[
-                SizedBox(height: 200,),
-                const Text(
-                  'Login',
-                  style: TextStyle(
-                    //fontFamily - colocar
-                    fontSize: 50,
-                    color: Colors.white,
-                  ),
-                ),
-                const SizedBox(height:70,),
-                  Form(
-                  child:Column(children: [ TextFormField(
-                    autocorrect: false,
-                    style: const TextStyle(
-                      fontSize: 22,
-                      color: Colors.white,
-                    ),
-                    decoration: const InputDecoration(
-                     
-                     enabledBorder: OutlineInputBorder(
-                        
-                        borderRadius: BorderRadius.all(Radius.circular(40), ),
-                        borderSide: BorderSide(color: Colors.cyan, width: 2),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(40), ),
-                        borderSide: BorderSide(color: Colors.cyan, width: 3),
-                      ),
-                      hintText: 'E-mail',
-                      hintStyle: TextStyle(color: Colors.grey),
-                      prefixIcon:Icon(Icons.email, color: Colors.cyan),
-
-                    ),
-                    keyboardType: TextInputType.emailAddress,
-                    validator: (value){
-                      if (value!.isEmpty){
-                        return 'Preencha o campo de email';
-                      } 
-                    },
-                  ),
-                  
-                  const SizedBox(height:30,),
-
-                  TextFormField(
-                    autocorrect: false,
-                    obscureText: true,
-                    
-                    style: const TextStyle(
-                      fontSize: 22,
-                      color: Colors.white,
-                    ),
-                    decoration: const InputDecoration(
-
-                       enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(40), ),
-                        borderSide: BorderSide(color: Colors.cyan, width: 2),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(40), ),
-                        borderSide: BorderSide(color: Colors.cyan, width: 3),
-                      ),
-                      hintText: 'Senha',
-                      hintStyle: TextStyle(color: Colors.grey),
-                      prefixIcon:Icon(Icons.lock_outline_rounded, color: Colors.cyan,),
-
-                    ),
-                    keyboardType: TextInputType.emailAddress,
-                    validator: (value){
-                      if (value!.isEmpty){
-                        return 'Preencha o campo de senha';
-                      } 
-                    },
-                  ),
-
-                  SizedBox(height: 10, ),
-                    const Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      
-                      children: [
-                        Text(
-                          'Esqueci minha senha     ',
-                          style: TextStyle(
-                            color: Colors.cyan,
-                            decoration: TextDecoration.underline,
-                            decorationColor: Colors.cyan,
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
-
-                    const SizedBox(height: 30),
-                      MaterialButton(
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-                        disabledColor: Colors.grey,
-                        color: Colors.cyan[600],
-                        onPressed: () {  },
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(horizontal: 100, vertical: 10),
-                          child: const Text(
-                            'Entrar',
-                            style: TextStyle(color: Colors.white, fontSize: 20),
-                          ),
-                        ),
-                                    /*onPressed: () {
-                                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder:
-                          (context) => const Menu()),
-                          );
-                                    },*/
-              ),
-
-              SizedBox(height: 250,),
-               const Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      
-                      children: [
-                        Text(
-                          'Você ainda não tem login?',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize:15,
-                          ),
-                        ),
-                        Text(
-                          '  Se cadastre!',
-                          style: TextStyle(
-                            color: Colors.cyan,
-                            decoration: TextDecoration.underline,
-                            decorationColor: Colors.cyan,
-                            fontSize: 15,
-                            
-                          ),
-                        ),
-                      ],
-                    ),
-              
-              
-      
-                  ],),
-                  
-                )
-            
-              ],)
-            ))
-          ],
-          )
-            ),
-        ),
-      );
+    return MaterialApp(
+      home: LoginScreen(),
+    );
   }
 }
 
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  // Variáveis locais para receber o texto
+  String ema = '';
+  String sen = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        color: Colors.grey[900],
+        child: Column(
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    const SizedBox(height: 200),
+                    const Text(
+                      'Login',
+                      style: TextStyle(
+                        fontSize: 50,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(height: 70),
+                    Form(
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            autocorrect: false,
+                            style: const TextStyle(
+                              fontSize: 22,
+                              color: Colors.white,
+                            ),
+                            decoration: const InputDecoration(
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(40)),
+                                borderSide:
+                                    BorderSide(color: Colors.cyan, width: 2),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(40)),
+                                borderSide:
+                                    BorderSide(color: Colors.cyan, width: 3),
+                              ),
+                              hintText: 'E-mail',
+                              hintStyle: TextStyle(color: Colors.grey),
+                              prefixIcon:
+                                  Icon(Icons.email, color: Colors.cyan),
+                            ),
+                            keyboardType: TextInputType.emailAddress,
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'Preencha o campo de email';
+                              }
+                              return null;
+                            },
+                            onChanged: (value) {
+                              setState(() {
+                                ema = value;
+                              });
+                            },
+                          ),
+                          const SizedBox(height: 30),
+                          TextFormField(
+                            autocorrect: false,
+                            obscureText: true,
+                            style: const TextStyle(
+                              fontSize: 22,
+                              color: Colors.white,
+                            ),
+                            decoration: const InputDecoration(
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(40)),
+                                borderSide:
+                                    BorderSide(color: Colors.cyan, width: 2),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(40)),
+                                borderSide:
+                                    BorderSide(color: Colors.cyan, width: 3),
+                              ),
+                              hintText: 'Senha',
+                              hintStyle: TextStyle(color: Colors.grey),
+                              prefixIcon:
+                                  Icon(Icons.lock_outline_rounded, color: Colors.cyan),
+                            ),
+                            keyboardType: TextInputType.emailAddress,
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'Preencha o campo de senha';
+                              }
+                              return null;
+                            },
+                            onChanged: (value) {
+                              setState(() {
+                                sen = value;
+                              });
+                            },
+                          ),
+                          const SizedBox(height: 10),
+                          const Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Text(
+                                'Esqueci minha senha     ',
+                                style: TextStyle(
+                                  color: Colors.cyan,
+                                  decoration: TextDecoration.underline,
+                                  decorationColor: Colors.cyan,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 30),
+                          MaterialButton(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30)),
+                            disabledColor: Colors.grey,
+                            color: Colors.cyan[600],
+                            onPressed: () {
+                              Login usu = Login(ema, sen);
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const Perfil(),
+                                  settings: RouteSettings(arguments: usu),
+                                ),
+                              );
+                            },
+                            child: Container(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 100, vertical: 10),
+                              child: const Text(
+                                'Entrar',
+                                style:
+                                    TextStyle(color: Colors.white, fontSize: 20),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 250),
+                          const Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Você ainda não tem login?',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                ),
+                              ),
+                              Text(
+                                '  Se cadastre!',
+                                style: TextStyle(
+                                  color: Colors.cyan,
+                                  decoration: TextDecoration.underline,
+                                  decorationColor: Colors.cyan,
+                                  fontSize: 15,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ flutter_application_1/lib/perfil.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/main.dart';
+
+class Perfil extends StatelessWidget {
+  const Perfil({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final usuario = ModalRoute.of(context)!.settings.arguments as Login;
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        iconTheme: const IconThemeData(color: Colors.white),
+        title: const Text('PERFIL'),
+        titleTextStyle: const TextStyle(
+          fontSize: 30,
+          color: Colors.white,
+        ),
+        backgroundColor: Colors.transparent,
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            const SizedBox(height: 50,),
+            const Icon(Icons.person_pin, size: 300, color: Colors.cyan,),
+            const Text(
+                  'Bem vindo(a)!',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+            const SizedBox(height: 30,),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: const [
+                Text(
+                  'Email:',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(usuario.email, style: const TextStyle(
+                  color: Colors.white, fontSize: 20,
+                ),)
+              ],
+            ),
+            const SizedBox(height: 20,),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: const [
+                Text(
+                  'Senha',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(usuario.senha, style: const TextStyle(
+                  color: Colors.white, fontSize: 20,
+                ),)
+              ],
+            ),
+         
+          ],
+        ),
+      ),
+    );
+  }
+}
